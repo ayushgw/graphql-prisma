d221f03ed667469cd4c3f9b0d215291cb03d2aa0
ayushgw <ayushgoswami@zoho.com>
2019-09-17 00:48:02 +0530

    prisma bindings

---
+++ prisma.js
@@ -0,0 +1,10 @@
+import { Prisma } from 'prisma-binding'
+
+const prisma = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: 'http://localhost:4466'
+})
+
+prisma.query.comments(null, '{ id text author { id name } }').then(data => {
+    console.log(JSON.stringify(data, undefined, 4));
+})
