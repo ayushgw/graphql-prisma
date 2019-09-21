f8bebc086bc0c02233736f9ac50ddc7074db2623
ayushgw <ayushgoswami@zoho.com>
2019-09-22 01:12:28 +0530

    Auth using JWT 🔐

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,19 +1,27 @@
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const Mutation = {
     async createUser(parent, args, { prisma }, info) {
-        if(args.data.password.length < 8) {
+        if (args.data.password.length < 8) {
             throw new Error('Password must be 8 characters or longer!')
         }
 
         const password = await bcrypt.hash(args.data.password, 10)
-        
-        return prisma.mutation.createUser({ 
+
+        const user = await prisma.mutation.createUser({
             data: {
                 ...args.data,
                 password
             }
-         }, info)
+        })
+
+        const token = jwt.sign({ userId: user.id }, 'myjwtsecret')
+
+        return {
+            user,
+            token
+        }
     },
     async deleteUser(parent, args, { prisma }, info) {
         return await prisma.mutation.deleteUser({
