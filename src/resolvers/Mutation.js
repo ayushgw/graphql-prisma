b9f3ca364ca4cc8e11a37f766cbf5607379aa9ce
ayushgw <ayushgoswami@zoho.com>
2019-09-21 14:54:08 +0530

    Mutations Done❕❕

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,3 @@
-import uuidv4 from 'uuid/v4'
-
 const Mutation = {
     async createUser(parent, args, { prisma }, info) {
         return prisma.mutation.createUser({ data: args.data }, info)
@@ -46,69 +44,37 @@ const Mutation = {
             data: args.data
         }, info)
     },
-    createComment(parent, args, { db, pubsub }, info) {
-        const userExists = db.users.some(user => user.id === args.data.author)
-        const isPostValid = db.posts.some(post => post.id === args.data.post && post.published)
-
-        if (!userExists || !isPostValid) {
-            throw new Error('Invalid user or post!')
-        }
-
-        const comment = {
-            id: uuidv4(),
-            ...args.data
-        }
-
-        db.comments.push(comment)
-
-        pubsub.publish(`comment ${comment.post}`, {
-            comment: {
-                mutation: 'CREATED',
-                data: comment
-            }
-        })
-
-        return comment
-    },
-    deleteComment(parent, args, { db, pubsub }, info) {
-        const commentIndex = db.comments.findIndex(comment => comment.id === args.id)
-
-        if (commentIndex === -1) {
-            throw new Error('Comment not found!')
-        }
-
-        // Splice returns an array containing the deleted items
-        const [comment] = db.comments.splice(commentIndex, 1)
-
-        pubsub.publish(`comment ${comment.post}`, {
-            comment: {
-                mutation: 'DELETED',
-                data: comment
-            }
-        })
-
-        return comment
-    },
-    updateComment(parent, args, { db, pubsub }, info) {
-        const { id, data } = args
-        const comment = db.comments.find(comment => comment.id === id)
-
-        if (!comment) {
-            throw new Error('No comment found!')
-        }
-
-        if (typeof data.text === 'string') {
-            comment.text = data.text
-
-            pubsub.publish(`comment ${comment.post}`, {
-                comment: {
-                    mutation: 'UPDATED',
-                    data: comment
+    createComment(parent, args, { prisma }, info) {
+        return prisma.mutation.createComment({
+            data: {
+                text: args.data.text,
+                author: {
+                    connect: {
+                        id: args.data.author
+                    }
+                },
+                post: {
+                    connect: {
+                        id: args.data.post
+                    }
                 }
-            })
-        }
-
-        return comment
+            }
+        }, info)
+    },
+    deleteComment(parent, args, { prisma }, info) {
+        return prisma.mutation.deleteComment({
+            where: {
+                id: args.id
+            }
+        }, info)
+    },
+    updateComment(parent, args, { prisma }, info) {
+        return prisma.mutation.updateComment({
+            where: {
+                id: args.id
+            },
+            data: args.data
+        }, info)
     }
 }
 
