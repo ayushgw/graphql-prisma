8378cf9be53c19b97bc70cb29f661db4a518df44
ayushgw <ayushgoswami@zoho.com>
2019-09-24 14:35:03 +0530

    Integrating Fragments

--- src/prisma.js
+++ src/prisma.js
@@ -1,9 +1,11 @@
 import { Prisma } from 'prisma-binding'
+import { fragmentReplacements } from './resolvers/index'
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: 'http://localhost:4466',
-    secret: 'graphqlcoursesupersecretkey'
+    secret: 'graphqlcoursesupersecretkey',
+    fragmentReplacements
 })
 
 export { prisma as default }
--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,14 +1,20 @@
 import getUserId from '../utils/getUserId'
 
 const User = {
-    email(parent, args, { request }, info) {
-        const userId = getUserId(request, false)
+    email: {
+        fragment: 'fragment userId on User { id }',
+        resolve(parent, args, { request }, info) {
+            const userId = getUserId(request, false)
 
-        if (userId && userId === parent.id) {
-            return parent.email
-        } else {
-            return null
+            if (userId && userId === parent.id) {
+                return parent.email
+            } else {
+                return null
+            }
         }
+    },
+    posts(parent, args, ctx, info) {
+        return parent.posts.filter(post => post.published)
     }
 }
 
